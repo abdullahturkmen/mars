0d4e6393e036cbbbdd5c0e1ea66a1fb1c7330a1f
Abdullah Türkmen <abdullahtrkmn13@gmail.com>
2019-11-13 12:19:17 +0300

    stil dosyaları atıldı

---
+++ assets/typed/demos.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 55,
+    backSpeed: 45,
+    startDelay: 0,
+    loop: true,
+    loopCount: Infinity,
+    onComplete: function(self) {
+      prettyLog('onComplete ' + self);
+    },
+    preStringTyped: function(pos, self) {
+      prettyLog('preStringTyped ' + pos + ' ' + self);
+    },
+    onStringTyped: function(pos, self) {
+      prettyLog('onStringTyped ' + pos + ' ' + self);
+    },
+    onLastStringBackspaced: function(self) {
+      prettyLog('onLastStringBackspaced ' + self);
+    },
+    onTypingPaused: function(pos, self) {
+      prettyLog('onTypingPaused ' + pos + ' ' + self);
+    },
+    onTypingResumed: function(pos, self) {
+      prettyLog('onTypingResumed ' + pos + ' ' + self);
+    },
+    onReset: function(self) {
+      prettyLog('onReset ' + self);
+    },
+    onStop: function(pos, self) {
+      prettyLog('onStop ' + pos + ' ' + self);
+    },
+    onStart: function(pos, self) {
+      prettyLog('onStart ' + pos + ' ' + self);
+    },
+    onDestroy: function(self) {
+      prettyLog('onDestroy ' + self);
+    }
+  });
+
+
+ 
+
+
+
+});
+
+function prettyLog(str) {
+  console.log('%c ' + str, 'color: green; font-weight: bold;');
+}
+
+function toggleLoop(typed) {
+  if (typed.loop) {
+    typed.loop = false;
+  } else {
+    typed.loop = true;
+  }
+}
